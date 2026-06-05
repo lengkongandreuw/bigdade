760c71b1a3b314dce2f0f613599a77f45a02b364
Andreuw Lengkong <98167235+lengkongandreuw@users.noreply.github.com>
2026-06-05 14:58:05 +0800

    Update WebGLCanvas.jsx

--- src/core/WebGLCanvas.jsx
+++ src/core/WebGLCanvas.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import { Canvas } from '@react-three/fiber'
-import ParticleBackground from '../webgl/environment/ParticleBackground'
+import ParticleBackground from '../webgl/environment/ParticleBackground' // Perhatikan tanda ../
 
 export default function WebGLCanvas() {
   return (
-    <Canvas camera={{ position: [0, 0, 5], fov: 60 }} gl={{ antialias: false, powerPreference: "high-performance" }}>
+    <Canvas 
+      camera={{ position: [0, 0, 5], fov: 60 }} 
+      gl={{ antialias: false, powerPreference: "high-performance" }}
+    >
       <ambientLight intensity={0.5} />
       <ParticleBackground />
     </Canvas>
